a996e55b343e5e0bc885911147764a439789268e
Aidan Leary <learyaidan@gmail.com>
2025-09-01 23:19:01 -0600

    merge quarter sine lut module into sine lut

--- src/rtl/SineLut.sv
+++ src/rtl/SineLut.sv
@@ -10,34 +10,41 @@ module SineLut
   input clk, 
   input rst, 
   input [ADDR_BITS - 1:0] addr_i, 
-  output logic signed [FRAC_BITS:0] sample_o
+  output reg signed [FRAC_BITS:0] sample_o
 ); 
 
-  localparam QLUT_ADDR_BITS = ADDR_BITS - 2;
-  localparam QLUT_SIZE = 2**QLUT_ADDR_BITS;
+  localparam SIZE = 2**ADDR_BITS;     // full lut size
+  localparam QLUT_ADDR_BITS = ADDR_BITS - 2; 
+  localparam QLUT_SIZE = 2**QLUT_ADDR_BITS;   // quarter lut size
 
-  logic qlut_addr;
-  wire qlut_sample;
+  localparam real PI = 3.1415926535;
+
+  logic [QLUT_ADDR_BITS - 1:0] qlut_addr;
+  logic [FRAC_BITS:0] qlut_sample;
+
+  reg [FRAC_BITS:0] qlut [0:2**QLUT_ADDR_BITS - 1];
+
+  initial begin 
+    for (int i = 0; i < QLUT_SIZE; i++) begin 
+      qlut[i] = $rtoi($sin(2 * PI * i / (4 * SIZE)) * 2**FRAC_BITS);
+    end
+  end
+
+  always_ff @(posedge clk, posedge rst) begin 
+    if (rst) begin 
+      sample_o <= 'h0;
+    end 
+    else begin 
+      // third and fourth quadrants: invert sample
+      sample_o <= addr_i[ADDR_BITS - 1] ? -qlut_sample : qlut_sample;
+    end
+  end
 
   // second and fourth quadrants: go backwards
   assign qlut_addr = addr_i[ADDR_BITS - 2] ? 
     (QLUT_SIZE - 1) - addr_i[QLUT_ADDR_BITS - 1:0] : 
     addr_i[QLUT_ADDR_BITS - 1:0];
 
-  // third and fourth quadrants: invert sample
-  assign sample_o = addr_i[ADDR_BITS - 1] ? -qlut_sample : qlut_sample;
-
-  QuarterSineLut 
-  #(
-    .ADDR_BITS(QLUT_ADDR_BITS), 
-    .FRAC_BITS(FRAC_BITS)
-  )
-  qlut 
-  (
-    .clk(clk), 
-    .rst(rst), 
-    .addr_i(qlut_addr), 
-    .sample_o(qlut_sample)
-  );
+  assign qlut_sample = qlut[qlut_addr];
 
 endmodule
