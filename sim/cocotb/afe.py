9796f29ec92be3033bdfc12dbe29f9f47dc78753
Aidan Leary <learyaidan@gmail.com>
2025-10-08 14:39:29 -0600

    add basic cocotb files

---
+++ sim/cocotb/afe.py
@@ -0,0 +1,112 @@
+import cocotb 
+from cocotb.queue import Queue 
+from cocotb.triggers import Timer 
+
+class HGA: 
+    '''
+    Model of HGA.
+    '''
+
+    def __init__(
+            self, 
+            in_queue, 
+            out_queue
+            ): 
+        self.in_queue = in_queue 
+        self.out_queue = out_queue 
+        self._bypass = True
+        self.gain = 100
+
+        cocotb.start_soon(self.run())
+
+    @property 
+    def bypass(self) -> bool: 
+        return self._bypass
+
+    @bypass.setter 
+    def bypass(self, val: bool): 
+        self._bypass = val
+
+    async def run(self): 
+        while True: 
+            in_val = await self.in_queue.get() 
+            if self._bypass: 
+                out_val = in_val 
+            else: 
+                out_val = in_val * self.gain
+            await self.out_queue.put(out_val)
+
+class PGA: 
+    '''
+    Model of PGA.
+    '''
+
+    def __init__(
+            self, 
+            init_gain, 
+            in_queue, 
+            out_queue
+            ): 
+        self._gain = init_gain 
+        self.in_queue = in_queue 
+        self.out_queue = out_queue 
+
+        cocotb.start_soon(self.run()) 
+
+        @property 
+        def gain(self) -> float: 
+            return self._gain 
+
+        @gain.setter 
+        def gain(self, val): 
+            self._gain = val
+
+        async def run(self): 
+            while True: 
+                in_val = await self.in_queue.get() 
+                self.out_queue.put(in_val * self._gain)
+
+class ADC: 
+    '''
+    Model of ADC.
+    '''
+
+    def __init__(
+            self, 
+            ref_V, 
+            bits, 
+            in_queue, 
+            out_queue
+            ): 
+        self.ref_V = ref_V 
+        self.min_val = 0 
+        self.max_val = 2**bits - 1 
+        self.in_queue = in_queue 
+        self.out_queue = out_queue 
+
+        cocotb.start_soon(self.run()) 
+
+        async def run(self) -> None: 
+            while True: 
+                in_val = await self.in_queue.git() 
+                out = int((in_val / self.ref_val) * self.max_val) 
+                await self.out_queue.put(min(max(self.min_val, out), self.max_val))
+
+class AFE: 
+    '''
+    Model of AFE. 
+    '''
+
+    def __init__(
+            self, 
+            in_queue, 
+            out_queue
+            ): 
+        self.in_queue = in_queue 
+        self.out_queue = out_queue 
+        self.pga_to_adc_queue = Queue() 
+        self.hga_to_pga_queue = Queue
+
+        self.hga = HGA(self.in_queue, self.hga_to_pga_queue)
+        self.pga = PGA(5, self.hga_to_pga_queue, self.pga_to_adc_queue)
+        self.adc = ADC(5, 12, self.pga_to_adc_queue, self.out_queue)
---
+++ sim/cocotb/test.py
@@ -0,0 +1,67 @@
+from afe import AFE 
+
+import cocotb 
+from cocotb.clock import Clock 
+from cocotb.queue import Queue 
+from cocotb.triggers import RisingEdge, Timer, ValueChange
+
+import numpy as np
+
+async def hga_set(dut, afe): 
+    '''
+    Listen for change in HGA bypass and update.
+    '''
+    while True: 
+        await ValueChange(dut.hga_bypass) 
+        if dut.hga_bypass.value == 0: 
+            afe.hga.bypass = False
+        else: 
+            afe.hga.bypass = True
+
+async def pga_set(dut, afe): 
+    '''
+    Listen for change in PGA gain and update.
+    '''
+    while True: 
+        await ValueChange(dut.pga_gain) 
+        afe.pga.gain = dut.pga_gain()
+
+async def generate_signal(afe): 
+    '''
+    Generate signal and feed into afe. 
+    '''
+    while True: 
+        sample_period = 1/5e6
+        time = 0
+        await Timer(sample_period, "sec")
+        time += sample_period
+        val = 0.01 * np.cos(2 * np.pi * 457e3 * time)
+        afe.in_queue.put(val)
+
+async def read_data(dut):
+    '''
+    Read data from the device.
+    '''
+    while True: 
+        await ValueChange(dut.some_val)
+        # collect value and place it into array? (plotting is a separete concern i think)
+
+@cocotb.test()
+async def test_dut(dut): 
+    '''
+
+    '''
+
+    clk_freq = 125e6
+    clk_period = 1/clk_freq
+    clk = Clock(dut.clk, clk_period, "sec")
+    cocotb.start_soon(clk.start()) 
+
+    afe_in_queue = Queue() 
+    afe_out_queue = Queue() 
+
+    afe = AFE(afe_in_queue, afe_out_queue) 
+
+    cocotb.start_soon(pga_set(dut, afe)) 
+    cocotb.start_soon(hga_set(dut, afe)) 
+    cocotb.start_soon(read_data(dut))
