e5e67d5af264e7df2319e64e42203ae561f5fa64
Aidan Leary <learyaidan@gmail.com>
2025-10-20 13:10:55 -0600

    convert to interleavd channel format

--- src/rtl/Comp2MagSqrd.sv
+++ src/rtl/Comp2MagSqrd.sv
@@ -6,65 +6,19 @@ module Comp2MagSqrd #(
   ) (
   input clk, 
   input rst, 
-  input [DW - 1:0] real_i, 
-  input [DW - 1:0] imag_i, 
+  input [DW - 1:0] data_i,  // interleaved real, imag
   input valid_i,
+  input last_i,
   output reg [2*DW - 1:0] mag_sqrd_o,
   output valid_o
   ); 
 
-  // 1. register inputs 
-  // 2. square real 
-  // 3. square imag and add 
-  // 4. register output
-
-  reg [DW - 1:0] real_reg;
-  reg [DW - 1:0] imag_reg;
+  reg [DW - 1:0] data_reg;
+  reg valid_reg;
+  reg last_reg;
 
   // multiplier
-  logic [DW - 1:0] sq_in; 
   logic [2*DW - 1:0] sq; 
-
-  /**************** 
-  * STATE MACHINE
-  * **************/
-
-  typedef enum {
-    IDLE, 
-    SQ_RE, 
-    SQ_IM 
-  } states_t;
-
-  reg [1:0] state; 
-  logic [1:0] next_state;
-
-  // state transition
-  always_ff @(posedge clk, posedge rst) begin 
-    if (rst) begin 
-      state <= IDLE;
-    end
-    else begin 
-      state <= next_state;
-    end
-  end
-
-  // next state
-  always_comb begin 
-    case (state) 
-      IDLE: begin 
-        next_state = valid_i ? SQ_RE : SQ_IM;
-      end
-      SQ_RE: begin 
-        next_state = SQ_IM;
-      end
-      SQ_IM: begin 
-        next_state = IDLE;
-      end
-      default: begin 
-        next_state = IDLE;
-      end
-    endcase
-  end
 
   /******************** 
   * Sequential 
@@ -73,51 +27,42 @@ module Comp2MagSqrd #(
   // input registers
   always_ff @(posedge clk, posedge rst) begin 
     if (rst) begin 
-      real_reg <= 'h0;
-      imag_reg <= 'h0;
+      data_reg <= 'h0;
+      valid_reg <= 1'h0; 
+      last_reg <= 1'h0;
     end
     else begin 
-      if ((state == IDLE) && valid_i) begin 
-        real_reg <= real_i; 
-        imag_reg <= imag_i;
-      end
+      data_reg <= data_i;
+      valid_reg <= valid_i; 
+      last_reg <= last_i;
     end
   end
 
-  // output register 
+  // output registers 
   always_ff @(posedge clk, posedge rst) begin 
     if (rst) begin 
       mag_sqrd_o <= 'h0; 
       valid_o <= 1'h0;
     end 
     else begin 
-      case (state) 
-        SQ_RE: begin 
+      if (valid_reg) begin 
+        // real (first channel)
+        if (!last_reg) begin 
           mag_sqrd_o <= sq;
+          valid_o <= 1'h0;
         end
-        SQ_IM: begin 
+        // imag (second channel)
+        else begin 
           mag_sqrd_o <= mag_sqrd_o + sq;
           valid_o <= 1'h1;
         end
-        default: begin 
-          valid_o <= 1'h0;
-        end
-      endcase
+      end
+      else begin 
+        valid_o <= 1'h0;
+      end
     end
   end
 
-  // select input to square multiplier
-  always_comb begin 
-    case (state)
-      SQ_IM: begin 
-        sq_in = imag_reg;
-      end
-      default: begin 
-        sq_in = real_reg;
-      end
-    endcase
-  end
-
-  assign sq = sq_in * sq_in;
+  assign sq = data_reg * data_reg;
 
 endmodule
