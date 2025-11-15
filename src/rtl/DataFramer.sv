1ea740c5b3de7cf0fec2d39b20f82d9137b53b12
Aidan Leary <learyaidan@gmail.com>
2025-11-14 19:15:58 -0700

    change to parameterized format

--- src/rtl/DataFramer.sv
+++ src/rtl/DataFramer.sv
@@ -1,33 +1,39 @@
 /*
 * Frames data to be sent out via UART.
 */
-module DataFramer (
+module DataFramer #(
+  NBYTES
+) (
   input clk, 
   input rst, 
-  input [15:0] data_i,
+  input [8*NBYTES - 1:0] data_i,
   input valid_i, 
+  input uart_fifo_full_i,
   output logic [7:0] uart_data,
-  output logic wr_en_o
-  ); 
+  output logic uart_wr_en_o
+); 
 
   localparam START_BYTE = 8'hAA; 
   localparam END_BYTE = 8'hBB;
 
-  reg [15:0] datareg;
+  localparam MAX_BYTE_COUNT = NBYTES - 1;
+
+  reg [8*NBYTES - 1:0] datareg;
+
+  reg [$clog2(MAX_BYTE_COUNT) - 1:0] byte_count;
 
   typedef enum {
     IDLE, 
     WR_START, 
-    WR_B0, 
-    WR_B1, 
+    WR_BYTES, 
     WR_END
   } states_t;
 
-  reg [2:0] state; 
-  logic [2:0] next_state;
+  reg [1:0] state; 
+  logic [1:0] next_state;
 
   // state transistion
-  always @(posedge clk, posedge rst) begin 
+  always_ff @(posedge clk, posedge rst) begin 
     if (rst) begin 
       state <= IDLE;
     end 
@@ -43,16 +49,16 @@ module DataFramer (
         next_state = valid_i ? WR_START : IDLE;
       end
       WR_START: begin 
-        next_state = WR_B0;
+        next_state = uart_fifo_full_i ? WR_START : WR_BYTES;
       end
-      WR_B0: begin 
-        next_state = WR_B1;
-      end
-      WR_B1: begin 
-        next_state = WR_END;
+      WR_BYTES: begin 
+        next_state = WR_BYTES; 
+        if ((byte_count == MAX_BYTE_COUNT) && !uart_fifo_full_i) begin 
+          next_state = WR_END;
+        end
       end
       WR_END: begin 
-        next_state = IDLE;
+        next_state = uart_fifo_full_i ? WR_END : IDLE;
       end
       default: begin 
         next_state = IDLE;
@@ -65,38 +71,47 @@ module DataFramer (
       datareg <= 'h0;
     end 
     else begin 
-      if ((state == IDLE) && valid_i) begin
-        datareg <= data_i;
+      if (state == IDLE) begin 
+        if (valid_i) begin
+          datareg <= data_i;
+        end
+      end 
+      else if (state == WR_BYTES) begin 
+        if (!uart_fifo_full_i) begin 
+          datareg <= datareg >> 8;
+        end
       end
     end
   end
 
-  // outputs
+  always_ff @(posedge clk, posedge rst) begin 
+    if (rst) begin
+      byte_count <= 'h0;
+    end 
+    else begin
+      if (state == WR_BYTES) begin 
+        byte_count <= byte_count + 1'h1;
+      end
+      else begin 
+        byte_count <= 'h0;
+      end
+    end
+  end
+
   always_comb begin 
-    wr_en_o = 1'h0;
-    uart_data = 'h0;
     case (state) 
       WR_START: begin 
         uart_data = START_BYTE;
-        wr_en_o = 1'h1;
-      end
-      WR_B0: begin 
-        uart_data = datareg[7:0];
-        wr_en_o = 1'h1;
       end 
-      WR_B1: begin 
-        uart_data = datareg[15:8];
-        wr_en_o = 1'h1;
-      end
       WR_END: begin 
-        uart_data = END_BYTE;
-        wr_en_o = 1'h1;
-      end
+        uart_data = END_BYTE; 
+      end 
       default: begin 
-        uart_data = 'h0;
-        wr_en_o = 1'h0;
+        uart_data = datareg[7:0];
       end
     endcase
   end
+
+  assign uart_wr_en_o = (state != IDLE) && !uart_fifo_full_i;
 
 endmodule
