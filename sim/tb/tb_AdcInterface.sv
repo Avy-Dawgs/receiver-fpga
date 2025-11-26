afbbf18bbc79dca38bade0d806a59b39cec46961
Aidan Leary <learyaidan@gmail.com>
2025-11-26 02:30:27 -0700

    update tb

--- sim/tb/tb_AdcInterface.sv
+++ sim/tb/tb_AdcInterface.sv
@@ -2,11 +2,12 @@ module tb_AdcInterface();
 
   reg sck; 
   reg rst; 
-  reg mosi; 
+  reg miso; 
 
   wire cs_n; 
   wire [11:0] data_o; 
   wire valid_o;
+  wire error_o;
 
   reg [11:0] data_i;
 
@@ -21,17 +22,18 @@ module tb_AdcInterface();
 
   initial begin 
     rst = 1; 
-    mosi = 1;
+    miso = 1;
     data_i = 11'h48F;
     #T;
     rst = 0;
 
-    wait(cs_n == 0);
+    @(negedge cs_n);
 
     for (int i = -1; i < 13; i++) begin 
-      wait(sck == 1) wait(sck == 0);
-      if (i >= 0 && i < 12) 
-        mosi = data_i[11 - i];
+      @(negedge sck);
+      if (i >= 0 && i < 12) begin
+        miso = data_i[11 - i];
+      end
     end
 
     #(50*T);
@@ -41,10 +43,11 @@ module tb_AdcInterface();
   AdcInterface dut (
     .sck(sck), 
     .rst(rst), 
-    .mosi(mosi), 
+    .miso(miso), 
     .cs_n(cs_n), 
     .data_o(data_o), 
-    .valid_o(valid_o)
+    .valid_o(valid_o), 
+    .error_o(error_o)
   );
 
 endmodule
