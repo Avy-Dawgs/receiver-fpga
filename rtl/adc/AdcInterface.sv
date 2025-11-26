9e92769c09ab270676b3465f06b344e78de86eb0
Aidan Leary <learyaidan@gmail.com>
2025-11-26 02:28:30 -0700

    shifting in on negedge now

--- rtl/adc/AdcInterface.sv
+++ rtl/adc/AdcInterface.sv
@@ -7,26 +7,25 @@ module AdcInterface (
   input miso,
   output reg cs_n,
   output reg [11:0] data_o, 
-  output valid_o, 
-  output error_o
+  output reg valid_o, 
+  output reg error_o
 ); 
 
-  reg [4:0] rising_edge_count;
+  reg [4:0] falling_edge_count;
   reg [11:0] shiftreg; 
-
+  reg [1:0] sample_count;
   reg error;     // start bit error
 
   logic start_conv; 
   logic start_acq;
 
-  reg [1:0] sample_count;
-
   // sample count
-  always_ff @(posedge sck, posedge rst) begin 
+  always_ff @(negedge sck, posedge rst) begin 
     if (rst) begin 
       sample_count <= 'd0;
     end 
     else if (start_acq) begin 
+      // saturate at 3
       if (sample_count < 'd3) begin 
         sample_count <= sample_count + 1'd1;
       end
@@ -34,7 +33,7 @@ module AdcInterface (
   end
 
   // shiftreg
-  always_ff @(posedge sck, posedge rst) begin 
+  always_ff @(negedge sck, posedge rst) begin 
     if (rst) begin 
       shiftreg <= 'h0;
     end 
@@ -43,17 +42,17 @@ module AdcInterface (
     end
   end
 
-  // rising edge count
-  always_ff @(posedge sck, posedge rst) begin 
+  // falling edge count
+  always_ff @(negedge sck, posedge rst) begin 
     if (rst) begin 
-      rising_edge_count <= 'd14;
+      falling_edge_count <= 'd14;
     end
     else begin 
       if (start_conv) begin 
-        rising_edge_count <= 'h0;
+        falling_edge_count <= 'h0;
       end
       else begin 
-        rising_edge_count <= rising_edge_count + 1'h1;
+        falling_edge_count <= falling_edge_count + 1'h1;
       end
     end
   end
@@ -73,13 +72,13 @@ module AdcInterface (
     end
   end
 
-  // start bit error
-  always_ff @(posedge sck, posedge rst) begin 
+  // error
+  always_ff @(negedge sck, posedge rst) begin 
     if (rst) begin 
       error <= 1'h0;
     end 
     else begin 
-      if (rising_edge_count == 'h0) begin 
+      if (falling_edge_count == 'h0) begin 
         error <= (miso != 1'h0);
       end
       else if (start_acq) begin 
@@ -91,21 +90,31 @@ module AdcInterface (
   end
 
   // output data reg
-  always_ff @(posedge sck, posedge rst) begin 
+  always_ff @(negedge sck, posedge rst) begin 
     if (rst) begin 
       data_o <= 'h0;
     end
     else begin 
-      if (start_acq) begin 
+      if (falling_edge_count == 'd12) begin 
         data_o <= shiftreg;
       end
     end
   end
 
-  assign start_conv = (rising_edge_count == 'd17);
-  assign start_acq = (rising_edge_count == 'd13);
+  always_ff @(posedge sck, posedge rst) begin 
+    if (rst) begin 
+      valid_o <= 1'h0;
+      error_o <= 1'h0;
+    end 
+    else if (sample_count == 'd3) begin 
+      if (falling_edge_count == 'd14) begin
+        valid_o <= 1'h1; 
+        error_o <= error;
+      end
+    end
+  end
 
-  assign valid_o = (sample_count == 'd3) && (rising_edge_count == 'd15);
-  assign error_o = valid_o && error;
+  assign start_conv = (falling_edge_count == 'd17);
+  assign start_acq = (falling_edge_count == 'd13);
 
 endmodule
